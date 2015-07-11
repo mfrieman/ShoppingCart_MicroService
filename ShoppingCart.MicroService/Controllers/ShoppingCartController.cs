fafbd02d271b805e686aa45db211e2f6aa631e5e
mfrieman <mfrieman@hotmail.com>
2015-07-11 12:02:26 -0400

    Initial project

---
+++ ShoppingCart.MicroService/Controllers/ShoppingCartController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ShoppingCart.MicroService.Models;
+namespace ShoppingCart.MicroService.Controllers
+{
+    public class ShoppingCartController : ApiController
+    {
+        // GET: api/ShoppingCart
+        public ShoppingCartDTO Get()
+        {
+            return ShoppingCartRepository.GetCart();
+        }
+                
+                
+  
+        // GET: api/ShoppingCart/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+
+
+
+
+
+    }
+}
---
+++ ShoppingCart.MicroService/Scripts/app/Main.js
@@ -0,0 +1,27 @@
+﻿function GetAllEmployees() {
+    //jQuery.support.cors = true;
+    $.ajax({
+        url: 'http://localhost:37000/api/shoppingcart',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            console.debug(data);
+            //$("#output").html(data.tostring());
+        },
+        error: function (x, y, z) {
+            alert(x + '\n' + y + '\n' + z);
+        }
+    });
+}
+
+
+// A $( document ).ready() block.
+
+$(document).ready(function () {
+    
+    $("#getdata").click(function(){
+        GetAllEmployees();
+});
+
+});
+
